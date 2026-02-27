6570c6559d442f1f82834cbbf2e6ddd28ff73a26
Ben Elliot <benjamintelliot@gmail.com>
2026-02-26 21:53:04 -0500

    First try at parser implementation.
    This is a basic naive implementation of a parser for this challenge.
    It's close but it isn't sorting the dates and it probably runs
    egregiously slow.
    
    First step is to get this working properly and then speeding it up.

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,37 @@
 
 namespace App;
 
-use Exception;
-
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $file = fopen($inputPath, 'r');
+        $finalMap = [];
+
+        while($csv = fgetcsv(stream: $file, escape: "\\")) {
+            if ($csv) {
+                $url = $csv[0];
+                $baseUrl = "https://stitcher.io";
+                $path = str_replace($baseUrl, '', $url);
+                $date = substr($csv[1], 0, 10);
+
+                if (!array_key_exists($path, $finalMap)) {
+                    $finalMap[$path] = [];
+                }
+
+                if (!array_key_exists($date, $finalMap[$path])) {
+                    $finalMap[$path][$date] = 1;
+                }
+                else {
+                    $finalMap[$path][$date] += 1;
+                }
+            }
+        }
+
+        fclose($file);
+
+        $outputFile = fopen($outputPath, 'w');
+        fwrite($outputFile, json_encode($finalMap, JSON_PRETTY_PRINT));
+        fclose($outputFile);
     }
 }
